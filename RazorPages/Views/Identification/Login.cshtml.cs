3a761af0078fa393207e23d10b452bf9dfeed599
Nikita Filyaev <nikita.filyaeff@yandex.ru>
2022-04-08 20:11:54 +0300

    Создание модели и структуры страницы  для логина

---
+++ RazorPages/Views/Identification/Login.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPages.Models;
+
+namespace RazorPages.Views.Identification
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credentials credential { get; set; }
+
+        public void OnGet()
+        {
+        }
+    }
+}
